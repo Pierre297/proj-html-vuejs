24e65ef68cf07054af7a92c6b6b1f2198b36dc35
Pier <pierfrancesco297@gmail.com>
2021-12-15 18:13:54 +0100

    tweet card e contatti

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -32,17 +32,68 @@
       </div>
     </div>
     <!-- CONTACT & INFO -->
-    <div></div>
+    <div class="divider"></div>
+    <div class="contacts-container">
+      <!-- constructions -->
+      <div class="construction">
+        <img src="../assets/construction_logo.png" alt="" />
+        <p>
+          We are specialists in the constructions of unique and exclusive
+          properties. Our work Inspires. We pride ourselves on delivering
+          outstanding quality and design for leading clients across the world
+        </p>
+        <button><a href="#">LEARN MORE</a></button>
+      </div>
+      <!-- tweet -->
+      <div>
+        <h3>LATEST TWEETS</h3>
+        <div class="tweetcard">
+          <Twitter
+            v-for="(tweet, tweetkey) in tweets"
+            :key="tweetkey"
+            :tweet="tweet"
+          />
+        </div>
+      </div>
+      <!-- contact -->
+      <div>
+        <h3>CONTACT US TODAY</h3>
+        <ul>
+          <li>
+            <i class="fas fa-globe"></i>Corporate Location 1600 Amphiteathre
+            Parkway London WC11BA
+          </li>
+          <li>
+            <i class="fas fa-home"></i>
+            Residential Location 9521 Broadsberry Avenue Paddington RC79ZA
+          </li>
+          <li><i class="fas fa-phone"></i>1.800.458.556 / 1.800.532.2112</li>
+          <li><i class="far fa-envelope"></i>info@your-domain.com</li>
+          <li>
+            <i class="far fa-clock"></i>Monday - Friday: 9:00 AM - 6:00 PM
+          </li>
+          <li>
+            <i class="far fa-clock"></i>Saturday - Sunday: 9:00 AM - 12:00 PM
+          </li>
+        </ul>
+      </div>
+      <!-- find us -->
+      <div>
+        <h3>FIND US</h3>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
 import Partnership from "./Partnership.vue";
+import Twitter from "./Twitter.vue";
 
 export default {
   name: "Footer",
   components: {
     Partnership,
+    Twitter,
   },
   data() {
     return {
@@ -61,6 +112,19 @@ export default {
         },
         {
           img: "home-logo9-219096700-320x202.png",
+        },
+      ],
+
+      tweets: [
+        {
+          profilepic: "3a74ce3d0532b7773b174c45ca3bd05a_bigger.png",
+          username: "ThemeFusion",
+          logo: "",
+          text: "Do you need sublime WordPress hosting for your next website? Take advantage of exclusive partner offers that we have secured just for you, & launch your site in seconds with #avanda on WP engine hosting & get 30% off this Black Friday bit.Ly/randomlink2020 ",
+          img: "En2TRxLW4AEiWUN.jpeg",
+          like: "",
+          share: "",
+          data: "",
         },
       ],
     };
@@ -142,6 +206,76 @@ export default {
   }
   .buttontext {
     margin-left: 40px;
+  }
+}
+
+// CONTACT & INFO
+.divider {
+  background-color: #3f3f3f;
+  height: 20px;
+}
+
+.contacts-container {
+  display: flex;
+  justify-content: space-between;
+  height: 550px;
+  background-color: #1e1e1e;
+}
+
+// constructions
+.construction {
+  text-align: start;
+  width: 400px;
+  color: #6f6f6f;
+
+  img {
+    margin: 30px;
+  }
+  p {
+    margin: 30px;
+  }
+  button {
+    margin: 30px;
+    height: 50px;
+    width: 200px;
+    background-color: #fed03d;
+    color: #534c35;
+
+    a {
+      text-decoration: none;
+      font-size: 20px;
+    }
+  }
+}
+
+// tweet
+.tweetcard {
+  width: 400px;
+  text-align: start;
+}
+// contacts
+.contacts-container {
+  h3 {
+    color: #fed03d;
+    font-size: 20px;
+    margin: 30px;
+    text-align: start;
+  }
+  ul {
+    list-style: none;
+    color: #6f6f6f;
+    text-align: start;
+    margin: 23px;
+
+    li {
+      margin: 13px;
+      width: 400px;
+      font-size: 15px;
+    }
+
+    i {
+      margin: 10px;
+    }
   }
 }
 </style>
---
+++ src/components/Twitter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tweetcard">
+    <img
+      class="profilepic"
+      :src="require('../assets/' + tweet.profilepic)"
+      alt=""
+    />
+    <strong>{{ tweet.username }}</strong>
+    <img class="logo" src="" alt="" />
+    <em>@{{ tweet.username }}</em>
+    <p>{{ tweet.text }}</p>
+    <img :src="require('../assets/' + tweet.img)" alt="" class="img-tweet" />
+    <img src="" alt="" class="like" />
+    <img src="" alt="" class="share" />
+    <span>{{ tweet.data }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Twitter",
+  props: {
+    tweet: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.tweetcard {
+  color: #6f6f6f;
+
+  em {
+    display: block;
+  }
+  p {
+    margin-top: 20px;
+  }
+  .img-tweet {
+    width: 400px;
+    margin-top: 10px;
+  }
+  .profilepic {
+    border-radius: 50%;
+    width: 30px;
+    margin: 0px 10px;
+  }
+}
+</style>
